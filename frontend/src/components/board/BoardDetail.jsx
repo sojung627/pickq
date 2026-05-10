50ed43a1794307ab4d48fbf3e15110e4898acf35
ppoya <temporary020627@gmail.com>
2026-05-10 22:04:08 +0900

    게시판 대공사 들어가기 전 생존용 세이브 커밋

--- frontend/src/components/board/BoardDetail.jsx
+++ frontend/src/components/board/BoardDetail.jsx
@@ -342,7 +342,7 @@ const BoardDetail = () => {
                         </div>
                         <div className="reply-content mb-2 text-sm text-[#222222] whitespace-pre-wrap">
                             {r.replyContent}
-                        </div>
+                        </div>   
                         {editReplyId === r.replyIdx && (
                           <div className="mt-2">
                             <textarea
